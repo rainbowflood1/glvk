67127761dce4003bdbb0140e53cdf842a8e0d1e1
Logical <88160870+rainbowflood1@users.noreply.github.com>
2026-04-21 18:13:39 -0400

    Updated glvk.hpp

--- include/glvk.hpp
+++ include/glvk.hpp
@@ -5,7 +5,22 @@
 // TYPES
 typedef unsigned int GLuint;
 typedef unsigned int GLenum;
+
+typedef signed size_t GLsizeiptr;
+typedef int_ptr_t GLingptr;
 // FUNCTIONS
 void glInit() {
+    // Used to initalise Vulkan
   
 }
+// OPENGL FUNCTIONS
+// Some of them are not finished
+
+// BUFFERS
+void glGenBuffers();
+void glBindBuffers();
+void glBufferData();
+// SHADERS
+void glCreateShader(GLenum type) {
+
+};
